2d89eea2bd9256b4e3a6b1a92392269da5d0aee0
Jonatan Attergrim <jonatana@k9tbt01.ki.se>
2022-05-18 16:32:43 +0200

    Added script file

---
+++ Study1/script.R
@@ -0,0 +1,128 @@
+
+## Load packages
+library(gmish)
+library(rofi)
+library(stringr)
+library(mikropml)
+library(dplyr)
+library(labelled)
+library(DBI)
+library(RMariaDB)
+library(dotenv)
+library(keyring)
+dotenv::load_dot_env()
+library(remotes)
+library(boot)
+library(DiagrammeR)
+library(tableone)
+library(table1)
+library(dplyr)
+library(kableExtra)
+library(lattice)
+library(caret)
+
+## Source functions
+source("../functions2.R")
+
+## Setup reading from the database
+dataset.names <- setNames(nm = c("swetrau", "fmp", "atgarder", "problem", "kvalgranskning2014.2017"))
+db.name <- "opportunities_for_improvement"
+scrambled <- FALSE ## Set to FALSE if you're working with the real data
+if (scrambled) dataset.names <- setNames(paste0(dataset.names, "_scrambled"), nm = dataset.names)
+if (scrambled) db.name <- paste0(db.name, "_scrambled")
+
+### Connect to server
+conn <- DBI::dbConnect(drv = RMariaDB::MariaDB(),
+                       user = 'jonatana',
+                       password = Sys.getenv("DB_PASSWORD"),
+                       db = "opportunities_for_improvement")
+
+
+## Read data
+datasets <- lapply(dataset.names, function(dataset.name) dbReadTable(conn = conn, name = dataset.name))
+attach(datasets)
+
+## If working with the real data we need to create the id variable for
+## matching by first reformatting the date and time of arrival (also
+## necessary for scrambled data) to the trauma unit so that it is the
+## same in all datasets, and then create the id variable by merging
+## the date and time variable with personal identifiers
+swetrau$arrival <- as.POSIXct(strptime(swetrau$DateTime_ArrivalAtHospital, format = "%Y-%m-%d %H:%M"))
+fmp$arrival <- as.POSIXct(strptime(fmp$Ankomst_te, format = "%Y%m%d %H:%M"))
+problem$arrival <- as.POSIXct(strptime(problem$Ankomst_te, format = "%Y%m%d %H:%M"))
+if (!scrambled) {
+  swetrau$id <- paste(swetrau$arrival, swetrau$PersonIdentity, swetrau$TempIdentity)
+  fmp$id <- paste(fmp$arrival, fmp$Personnummer, fmp$Reservnummer)
+  problem$id <- paste(problem$arrival, problem$Personnummer, problem$Reservnummer)
+}
+
+## Combine datasets
+
+combined.datasets <- merge(fmp, problem, by = "id", all.x = TRUE)
+combined.datasets <- merge(combined.datasets, swetrau, by = "id", all.x = TRUE)
+
+## Create OFI collumn
+combined.datasets[,"Fr1-14"] <- combined.datasets$Fr1.14
+combined.datasets$ofi <- create_ofi(combined.datasets)
+
+### Cleaning data
+
+### Clean previous audit filters
+dataset.clean.af <- clean_audit_filters(combined.datasets)
+
+## Only those who are done with VK / Dodsfallskonferans
+
+dataset.clean.af <- subset(dataset.clean.af, dataset.clean.af$tra_DodsfallsanalysGenomford == 1 | dataset.clean.af$VK_avslutad == "Yes" )
+
+## Separate and store cases without known outcome
+missing.outcome <- is.na(dataset.clean.af$ofi)
+n.missing.outcome <- sum(missing.outcome)
+dataset.clean.af <- dataset.clean.af[!missing.outcome, ]
+
+## Clean predictors (Correct NA classification and predictor choice)
+clean.dataset <- clean_predictor(dataset.clean.af)
+
+## Imputation
+imputed.dataset <- imputation(clean.dataset)
+
+############## 
+## Codes for sample size
+##############
+
+## Load the package referenced by @riley2020
+library(pmsampsize)
+# Binary outcome
+type <- "b"
+## Observations in database
+n.swetrau <- nrow(swetrau)
+n.2017.2021 <- nrow(clean.dataset)
+## Calculate sum of OFI - NEED to change this?
+ofi <- sum(combined.datasets$ofi == "Yes")
+## Prevalence
+prevalence <- ofi/n.2017.2021
+## Unknown, but using the recommendations in @riley2020 we set
+## Nagelkerke R2 to 0.15 and assume an event fraction of 0.01
+rsquared <- 0.11
+## 30 candidate predictors
+parameters <- 15
+## Sample size
+#sample.size <- pmsampsize(type = type,
+#                         rsquared = rsquared,
+#                        parameters = parameters,
+#                       prevalence = prevalence)
+## Utility functions
+fmt_p <- function(x, n) sprintf("%.0f", round(x*100/n))
+
+## Sort data according to time and create 80% training vector for training set
+## selection
+clean.dataset <-clean.dataset[order(clean.dataset$arrival), ]
+tv <- c(1:round(nrow(clean.dataset)*0.8, digits = 0))
+
+
+## Result calculations ##
+
+boot.results <- boot(data = combined.datasets, statistic = Bootstrap, R = 6)
+
+# test for result extraction. 
+#accuricy.ci <- boot.ci(boot.results, type = "norm", index = 1)
+#ci.auc <- boot.ci(boot.results, type = "norm", index = 2)
