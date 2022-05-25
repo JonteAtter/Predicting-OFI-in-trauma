7c06127ba102217059cd7016b693f6bda027f92e
Martin Gerdin Wärnberg <marting@k9tbt01.ki.se>
2022-05-25 21:46:36 +0200

    Implement new rofi functions to import and merge data and make script.R run

--- Study1/functions2.R
+++ Study1/functions2.R
@@ -182,27 +182,28 @@ imputation <- function(dataset) {
 preprocess_data <- function(data) {
   preprocessed.data <- mikropml::preprocess_data(data, outcome_colname = "ofi", to_numeric = FALSE)
   return (preprocessed.data$dat_transformed)
-  
+}
+
 ###############
 ## Bootstrap ##  
 ###############
   
-  Bootstrap <- function(data, index) {
+bootstrap <- function(data, index) {
     random.datasets <- data[index,]
     
-    #if (!is.null(index)) {
-    #  random.datasets <- data[index,]. ### Fungerade ej direkt, och tog bort i ett försök att minimera felkällor. 
-    #}
+    ##if (!is.null(index)) {
+    ##  random.datasets <- data[index,]. #### Fungerade ej direkt, och tog bort i ett försök att minimera felkällor. 
+    ##}
     
     ## Create OFI collumn
-    random.datasets[,"Fr1-14"] <- random.datasets$Fr1.14
-    random.datasets$ofi <- create_ofi2(random.datasets)
+     random.datasets$ofi <- rofi::create_ofi(random.datasets)
     
-    ### Clean previous audit filters
+    ## Clean previous audit filters
     random.clean.af <- clean_audit_filters(random.datasets)
     
     ## Only those who are done with VK / Dodsfallskonferans
     random.clean.af <- subset(random.clean.af, random.clean.af$tra_DodsfallsanalysGenomford == 1 | random.clean.af$VK_avslutad == "Yes" )
+
     ## Separate and store cases without known outcome
     missing.outcome <- is.na(random.clean.af$ofi)
     random.clean.af <- random.clean.af[!missing.outcome, ]
@@ -210,8 +211,8 @@ preprocess_data <- function(data) {
     ## Clean predictors (Correct NA classification and predictor choice)
     clean.random.dataset <- clean_predictor(random.clean.af)
     
-    #clean.random.dataset <-clean.random.dataset[order(clean.random.dataset$arrival), ]
-    #tv <- c(1:round(nrow(clean.random.dataset)*0.8, digits = 0))
+    ## clean.random.dataset <-clean.random.dataset[order(clean.random.dataset$arrival), ]
+    ## tv <- c(1:round(nrow(clean.random.dataset)*0.8, digits = 0))
     
     ## Imputation
     imputed.dataset <- imputation(clean.random.dataset)
@@ -249,7 +250,7 @@ preprocess_data <- function(data) {
     labels <- test.data$ofi
     test.data$ofi <- NULL
     
-    ### log reg
+    ## log reg
     final.model.lr <- results.lr$trained_model$finalModel
     prediction.lr <- predict(final.model.lr, newx = as.matrix(test.data), s = 0.1, type = "response")
     pred.lr <- ROCR::prediction(prediction.lr, labels)
@@ -257,9 +258,9 @@ preprocess_data <- function(data) {
     auc.lr <- unlist(auc.lr)
     accuracy.lr <- ROCR::performance(pred.lr, measure = "acc")@y.values
     accuracy.lr <- mean(unlist(accuracy.lr))
-    #ici.lr <- ici(prediction.lr, labels) # Fungerar ej direkt men ej hunnit felsöka 100%
+    ##ici.lr <- ici(prediction.lr, labels) ## Fungerar ej direkt men ej hunnit felsöka 100%
     
-    ### Randrom forest
+    ## Randrom forest
     final.model.forest <- results.forest$trained_model$finalModel
     pred.rf <- predict(final.model.forest, newdata = as.matrix(results.forest$test_data), predict.all = TRUE)
     pred.forest <- pred.rf$aggregate
@@ -268,14 +269,14 @@ preprocess_data <- function(data) {
     accuracy.forest <- ROCR::performance(prediction.forest3, measure = "acc")@y.values
     accuracy.forest <- mean(unlist(accuracy.forest))
     
-    ### SVM
-    #final.model.vector.machine <- results.vector.machine$trained_model$finalModel
-    #prediction.vector.machine <- predict(final.model.vector.machine, newx = as.matrix(test.data), s = 0.1, type = "response")
-    #pred.vector.machine <- ROCR::prediction(prediction.vector.machine, labels)
-    #auc.vector.machine <- ROCR::performance(pred.vector.machine, measure = "auc")@y.values
-    #accuracy.vector.machine <- ROCR::performance(pred.vector.machine, measure = "acc")@y.values
+### SVM
+    ## final.model.vector.machine <- results.vector.machine$trained_model$finalModel
+    ## prediction.vector.machine <- predict(final.model.vector.machine, newx = as.matrix(test.data), s = 0.1, type = "response")
+    ## pred.vector.machine <- ROCR::prediction(prediction.vector.machine, labels)
+    ## auc.vector.machine <- ROCR::performance(pred.vector.machine, measure = "auc")@y.values
+    ## accuracy.vector.machine <- ROCR::performance(pred.vector.machine, measure = "acc")@y.values
     
-    ### xgboost
+    ## xgboost
     final.model.boost <- results.boost$trained_model$finalModel
     prediction.boost <- xgboost:::predict.xgb.Booster(final.model.boost, newdata = as.matrix(test.data), type = "response")
     pred.boost <- ROCR::prediction(prediction.boost, labels)
@@ -284,9 +285,9 @@ preprocess_data <- function(data) {
     accuracy.boost <- ROCR::performance(pred.boost, measure = "acc")@y.values
     accuracy.boost <- mean(unlist(accuracy.boost))
     
-    ### Result summary
-    ## Delta AUC 
+    ## Result summary
     
+    ## Delta AUC 
     lr.rf.auc <- abs(as.numeric(auc.lr) - as.numeric(auc.forest))
     lr.boost.auc <- abs(as.numeric(auc.lr) - as.numeric(auc.boost))
     rf.boost.auc <- abs(as.numeric(auc.boost) - as.numeric(auc.forest))
@@ -299,7 +300,6 @@ preprocess_data <- function(data) {
              delta.rf.boost.auc = rf.boost.auc)
     
     ## Delta accuracy
-    
     lr.rf.acc <- abs(as.numeric(accuracy.lr) - as.numeric(accuracy.forest))
     lr.boost.acc <- abs(as.numeric(accuracy.lr) - as.numeric(accuracy.boost))
     rf.boost.acc <- abs(as.numeric(accuracy.boost) - as.numeric(accuracy.forest))
@@ -313,18 +313,18 @@ preprocess_data <- function(data) {
     
     boot.result <- c(accuracy,auc)
     
-    # if (!boot) {
-    #  boot.result <- list(boot.result,
-    #                     lr.model = results.lr,
-    #                    rf.model = results.forest,
-    #                   boost.model = results.boost)
-    # }
+    ## if (!boot) {
+    ##  boot.result <- list(boot.result,
+    ##                     lr.model = results.lr,
+    ##                    rf.model = results.forest,
+    ##                   boost.model = results.boost)
+    ## }
     
     if (!dir.exists("out"))
-      dir.create("out")
+        dir.create("out")
     filename <- paste0("out/boot.result.", gsub(".", "", as.numeric(Sys.time()), fixed = TRUE), ".Rds")
     saveRDS(boot.result, filname)
     message("Bootstrap analysis completed and results saved")
     return(boot.result)
-  }
 }
+
--- Study1/script.R
+++ Study1/script.R
@@ -1,58 +1,18 @@
-
 ## Load packages
-packages <- c("gmish", "rofi", "stringr", "mikropml", "dplyr", "labelled", "DBI", "RMariaDB", "dotenv", "keyring", "remotes", "boot", "DiagrammeR", "tableone", "table1", "dplyr", "kableExtra", "lattice", "caret")
-for (package in packages) {
-    if (!require(package, quietly = TRUE)) {
-        install.packages(package)
-    }
-    libraray(package)
-}
-
-dotenv::load_dot_env
+packages <- c("Gmisc", "stringr", "mikropml", "dplyr", "labelled", "DBI", "RMariaDB", "dotenv", "keyring", "remotes", "boot", "DiagrammeR", "tableone", "table1", "dplyr", "kableExtra", "lattice", "caret")
+for (package in packages) library(package, character.only = TRUE)
 
 ## Source functions
-source("../functions2.R")
+source("functions2.R")
 
-## Setup reading from the database
-dataset.names <- setNames(nm = c("swetrau", "fmp", "atgarder", "problem", "kvalgranskning2014.2017"))
-db.name <- "opportunities_for_improvement"
-scrambled <- FALSE ## Set to FALSE if you're working with the real data
-if (scrambled) dataset.names <- setNames(paste0(dataset.names, "_scrambled"), nm = dataset.names)
-if (scrambled) db.name <- paste0(db.name, "_scrambled")
+## Import data
+datasets <- rofi::import_data()
 
-### Connect to server
-conn <- DBI::dbConnect(drv = RMariaDB::MariaDB(),
-                       user = 'jonatana',
-                       password = Sys.getenv("DB_PASSWORD"),
-                       db = "opportunities_for_improvement")
-
-
-## Read data
-datasets <- lapply(dataset.names, function(dataset.name) dbReadTable(conn = conn, name = dataset.name))
-attach(datasets)
-
-## If working with the real data we need to create the id variable for
-## matching by first reformatting the date and time of arrival (also
-## necessary for scrambled data) to the trauma unit so that it is the
-## same in all datasets, and then create the id variable by merging
-## the date and time variable with personal identifiers
-swetrau$arrival <- as.POSIXct(strptime(swetrau$DateTime_ArrivalAtHospital, format = "%Y-%m-%d %H:%M"))
-fmp$arrival <- as.POSIXct(strptime(fmp$Ankomst_te, format = "%Y%m%d %H:%M"))
-problem$arrival <- as.POSIXct(strptime(problem$Ankomst_te, format = "%Y%m%d %H:%M"))
-if (!scrambled) {
-  swetrau$id <- paste(swetrau$arrival, swetrau$PersonIdentity, swetrau$TempIdentity)
-  fmp$id <- paste(fmp$arrival, fmp$Personnummer, fmp$Reservnummer)
-  problem$id <- paste(problem$arrival, problem$Personnummer, problem$Reservnummer)
-}
-
-## Combine datasets
-
-combined.datasets <- merge(fmp, problem, by = "id", all.x = TRUE)
-combined.datasets <- merge(combined.datasets, swetrau, by = "id", all.x = TRUE)
+## Merge data
+combined.datasets <- rofi::merge_data(datasets)
 
 ## Create OFI collumn
-combined.datasets[,"Fr1-14"] <- combined.datasets$Fr1.14
-combined.datasets$ofi <- create_ofi(combined.datasets)
+combined.datasets$ofi <- rofi::create_ofi(combined.datasets)
 
 ### Cleaning data
 
@@ -60,7 +20,6 @@ combined.datasets$ofi <- create_ofi(combined.datasets)
 dataset.clean.af <- clean_audit_filters(combined.datasets)
 
 ## Only those who are done with VK / Dodsfallskonferans
-
 dataset.clean.af <- subset(dataset.clean.af, dataset.clean.af$tra_DodsfallsanalysGenomford == 1 | dataset.clean.af$VK_avslutad == "Yes" )
 
 ## Separate and store cases without known outcome
@@ -83,7 +42,7 @@ tv <- c(1:round(nrow(clean.dataset)*0.8, digits = 0))
 ## Result calculations ##
 if (file.exists("out/boot.results.Rds"))
     file.remove("out/boot.results.Rds")
-boot.results <- boot(data = combined.datasets, statistic = Bootstrap, R = 6)
+boot.results <- boot(data = combined.datasets, statistic = bootstrap, R = 6)
 saveRDS("out/boot.results.Rds")
 if (file.exists("out/boot.results.Rds"))
     message("All bootstraps completed and results saved to disk")
