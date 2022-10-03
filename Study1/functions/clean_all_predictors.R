bf9052c9329f765a1f7d80d2c05e78afcc78a6dd
Jonatan Attergrim <jonatana@k9tbt01.ki.se>
2022-10-03 20:29:10 +0200

    Uppdated clean_all_predictors

--- Study1/functions/clean_all_predictors.R
+++ Study1/functions/clean_all_predictors.R
@@ -2,9 +2,8 @@
 ### Clean predictors ##
 #######################
 #
-# Todo: Clean all possible predictors
-# Integrate cont/cat.var.no.99 into cont/cat.var instead of having two different vectors.
-# something beside summary() to check correct formats?
+# TODO:something beside summary() to check correct formats?
+# Fix dates?
 
 clean_all_predictors <- function(data) {
 
@@ -12,82 +11,57 @@ clean_all_predictors <- function(data) {
 cont.var<- c("pt_age_yrs","pre_gcs_sum","pre_gcs_motor","ed_gcs_sum","ed_gcs_motor",
              "pre_sbp_value","ed_sbp_value","pre_rr_value","ed_rr_value","ed_be_art",
              "ed_inr","hosp_vent_days","hosp_los_days","res_gos_dischg","ISS","NISS",
-             "NumberOfActions","NumberOfInjuries")  
+             "NumberOfActions","NumberOfInjuries","iva_dagar_n","iva_vardtillfallen_n")  
 
 ###cat 
 cat.var <- c("Gender","pt_Gender","ed_be_art_NotDone","hosp_dischg_dest","res_survival",           
              "inj_dominant","inj_mechanism","inj_intention","pt_asa_preinjury","pre_card_arrest",
              "pre_sbp_rtscat","ed_sbp_rtscat","pre_rr_rtscat","ed_rr_rtscat","ed_inr_NotDone",
              "host_vent_days_NotDone","pre_provided","pre_intubated","pre_intub_type",
-             "ed_intubated","ed_intub_type","ed_tta","ed_emerg_proc_other","ed_emerg_proc","pre_transport")
+             "ed_intubated","ed_intub_type","ed_tta","ed_emerg_proc_other","ed_emerg_proc","pre_transport",
+             "Deceased","bedomn_primar_granskning","Riktlinje",
+             "waran_beh_vid_ank","noak_vid_ankomst",
+             "TraumaAlarmCriteria","TraumaAlarmAtHospital","AlarmRePrioritised",
+             "FirstTraumaDT_NotDone","ISS_less_15_trauma_1_2","korrekt_triage",
+             "tid_skadeplats_moore_20min","host_transfered", "host_care_level",
+             "tra_DodsfallsanalysGenomford","ISS_moore_15_trauma_3","GCS_less13_DT_moore_120",
+             "Kön","Tr_Nivå","tillavdelning_Direkt_eft_TE_AKM","IVA_efter_TE",
+             "IVA_eft_avd","Flyttad_till_avd_eft_iva","Problemomrade_.FMP","ofi")
 
-######
-# get the collumns not to be checked.
-######
-test.data <- remove_columns(data)
-test.data <- as.data.frame(test.data)
-test.data <- test.data[,-grep("Fr", colnames(test.data))]
-test.data <- test.data[,-grep("DateTime_", colnames(test.data))]
-test.data <- test.data[,-grep("dt_", colnames(test.data))]
+vars <- c(cat.var,cont.var)
+
+#############################################
+# Change obvios values to NA (Like 999 etc) #
+#############################################
 
 
-######## Need to check these collumns, in the future so they follow the swetrau manual and is formated correctly. 
+## Vars.99 = Vars that that should contain 99
+# "ed_emergency_proc" 99 == No action
+# "TraumaAlarmAtHospital" 99 == No trauma alarm
+# Note that ed_gcs_sum 99 == Intubated in a prehospital seting but we need to remove to calculate correct median/mean
+var.99 <- c("ed_emergency_pro","TraumaAlarmAtHospital","pt_age_yrs","pre_sbp_value","ed_sbp_value","hosp_vent_days","hosp_los_days","iva_dagar_n","iva_vardtillfallen_n")
 
-columnnames <- colnames(subset(test.data,
-                               select = -c(did,tra_id,pat_id,Sjukhuskod,PersonIdentity,TempIdentity,DOB,Deceased,DeceasedDate,
-                                           TraumaAlarmCriteria,TraumaAlarmAtHospital,AlarmRePrioritised,FirstTraumaDT_NotDone,   
-                                           host_transfered, host_care_level,Date_Discharged,tra_DodsfallsanalysGenomford,
-                                           id,arrival,ISS_moore_15_trauma_3,bedomn_primar_granskning,pat_personnummer,
-                                           ISS_less_15_trauma_1_2,pat_TempPersonnummer,korrekt_triage,tid_skadeplats_moore_20min,
-                                           tid_skadeplats,GCS_less13_DT_moore_120,Riktlinje,origin,Ankomst_te,Personnummer,
-                                           Reservnummer,Kön,Tr_Nivå,tillavdelning_Direkt_eft_TE_AKM,IVA_efter_TE,IVA_eft_avd,
-                                           iva_dagar_n,iva_vardtillfallen_n,Död.datum,uppfolj_30_dgr,Klar,SweTr,
-                                           Flyttad_till_avd_eft_iva,inlagd_swetrau,waran_beh_vid_ank,noak_vid_ankomst,
-                                           Problemomrade_.FMP,ofi )
-)
-)
-
-#####
-# Change obvios values to NA (Like 999)
-#####
-
-cont.var.no.99<- c("pre_gcs_sum","pre_gcs_motor","ed_gcs_sum","ed_gcs_motor","pre_rr_value","ed_rr_value","ed_be_art",
-                   "ed_inr","ISS","NISS","NumberOfActions","NumberOfInjuries") 
-
-# Note that ED_GCS 99 == Intubated in a prehospital seting but we need to remove to calculate correct median/mean
-
-cat.var.no.99 <- c("Gender","pt_Gender","ed_be_art_NotDone","hosp_dischg_dest","res_survival",           
-             "inj_dominant","inj_mechanism","inj_intention","pt_asa_preinjury","pre_card_arrest",
-             "pre_sbp_rtscat","ed_sbp_rtscat","pre_rr_rtscat","ed_rr_rtscat","ed_inr_NotDone",
-             "host_vent_days_NotDone","pre_provided","pre_intubated","pre_intub_type",
-             "ed_intubated","ed_intub_type","ed_tta","ed_emerg_proc_other","res_gos_dischg",
-             "pre_transport")
-
-# Note that ed_emergency_proc 99 == No action
-
-test.data <- data[,columnnames]
-
+test.data <- data[,vars]
 test.data[test.data == 999] <- NA
 test.data[test.data == 9999] <- NA #### Consider removing since 9999 actually means not relevant?
-## 99 is only rarely something of meaning
-test.data[, cont.var.no.99][test.data[, cont.var.no.99] == 99]<- NA 
-test.data[, cat.var.no.99][test.data[, cat.var.no.99] == 99] <- NA
+test.data[, -which(names(test.data) %in% var.99)][test.data[, -which(names(test.data) %in% var.99)] == 99 ] <- NA
 
 ##### Fix formating to factor/numeric for easy screening
-clean.data <- test.data
 
-clean.data[cat.var] <- lapply(clean.data[cat.var], factor)
-clean.data$ed_be_art = str_replace(clean.data$ed_be_art,",",".")
-clean.data$ed_inr = str_replace(clean.data$ed_inr,",",".")
-clean.data$ed_inr <- as.numeric(clean.data$ed_inr)
-clean.data$ed_be_art <- as.numeric(clean.data$ed_inr)
-clean.data$NumberOfActions <- as.numeric(clean.data$NumberOfActions)
-clean.data$NumberOfInjuries <- as.numeric(clean.data$NumberOfInjuries)
+formated.data <- test.data
+formated.data[cat.var] <- lapply(formated.data[cat.var], factor)
+formated.data$ed_be_art = str_replace(formated.data$ed_be_art,",",".")
+formated.data$ed_inr = str_replace(formated.data$ed_inr,",",".")
+formated.data$ed_inr <- as.numeric(formated.data$ed_inr)
+formated.data$ed_be_art <- as.numeric(formated.data$ed_inr)
+formated.data$NumberOfActions <- as.numeric(formated.data$NumberOfActions)
+formated.data$NumberOfInjuries <- as.numeric(formated.data$NumberOfInjuries)
 
 ###### I Used summary to check each category, to my knowledge they follow the swetrau-manual
 ###### Need to add some better screening tool
 
-data[columnnames] <- clean.data
+data[vars] <- formated.data
+
 return(data)
 
 }
--- Study1/script.R
+++ Study1/script.R
@@ -9,6 +9,7 @@
 packages <- c("rofi","Gmisc", "stringr", "mikropml", "dplyr", "labelled", "DBI", "RMariaDB", "dotenv", "keyring", "remotes", "boot", "DiagrammeR", "tableone", "table1", "dplyr", "kableExtra", "lattice", "caret")
 for (package in packages) library(package, character.only = TRUE)
 
+## Load functions
 source("functions/functions.R")
 
 ## Import data
@@ -16,6 +17,7 @@ datasets <- rofi::import_data()
 
 ## Merge data
 combined.datasets <- rofi::merge_data(datasets)
+
 ## Create OFI collumn
 combined.datasets$ofi <- rofi::create_ofi(combined.datasets)
 
@@ -25,18 +27,15 @@ combined.datasets$ofi <- rofi::create_ofi(combined.datasets)
 ### Clean audit filters
 dataset.clean.af <- clean_audit_filters(combined.datasets)
 
-## Only those who are done with VK / Dodsfallskonferans
-dataset.clean.af <- subset(dataset.clean.af, dataset.clean.af$tra_DodsfallsanalysGenomford == 1 | dataset.clean.af$VK_avslutad == "Yes" )
-
 ## Separate and store cases without known outcome
 missing.outcome <- is.na(dataset.clean.af$ofi)
 n.missing.outcome <- sum(missing.outcome)
 dataset.clean.af <- dataset.clean.af[!missing.outcome, ]
 
-## Need to check if further/all predictors follow swetrau. Check function.
+## Fix formating and remove wrong values like 999
 clean.dataset <- clean_all_predictors(dataset.clean.af)
 
-#### Remove columns not used for prediction, expand?
+#### Remove columns not used for prediction - NEED TO EXPAND
 smaller.data <- remove_columns(clean.dataset)
 
 ## Imputation ---- OLD Function. Need consensus on how we imputate. 
@@ -57,7 +56,7 @@ preprocessed.data <- preprocess_data(smaller.data)
 
 #### Boot test
 #
-#results.boot3 <- boot(data=combined.datasets, statistic=bootstrap,
+#results.boot3 <- boot(data=preprocessed.data, statistic=bootstrap,
 #                      R=100)
 #
 # Change index to access different performance meassures
